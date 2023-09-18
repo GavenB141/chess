b6d63fd3eb52e343d8f04cf87538fade93ad2063
Gaven Behrends <gzbehrends@gmail.com>
2023-09-18 17:34:39 -0500

    Title change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Shess',
+  title: 'Chess',
   description: 'A WIP chess engine',
 }
 
